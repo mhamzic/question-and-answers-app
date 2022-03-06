7dc25f53aac2b5ea1c32a4664509e7a537650bf5
mhamzic <mhamzic@gmail.com>
2022-03-06 14:25:04 +0100

    changes before deploying

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,8 +1,12 @@
 const dotenv = require("dotenv").config();
 
-
 const { Pool } = require("pg");
-const pool = new Pool();
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : "");
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
